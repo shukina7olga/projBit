25b86b0437a6ba4de072aa09b2f364871f7eebd1
shukina7olga <olga.shchukina.99@mail.ru>
2022-02-16 14:37:30 +0300

    task1 version4

--- backend.php
+++ backend.php
@@ -1,71 +1,23 @@
 <?php 
+   echo "<link rel='stylesheet' href='style.css'>";
 
    $name = 'Bars';
    $login = 'barsum';
    $pass = '123';
 
-   //$nameId = $_POST['nameId']; если можно обратиться типа nameId.color 
-   //$loginId = $_POST['loginId'];
-   //$passId = $_POST['passId'];
-
-
-   if($pass !== $_POST['pass'] && $login !== $_POST['login']) {
+   if($pass !== $_POST['pass'] || $login !== $_POST['login']) {
       echo 
-      "<div style='width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column;'>
-         <h2>Неверный логин и пароль!</h2>
-         <a style='display: inline-block; 
-            text-decoration: none; 
-            background: #cbf95a;
-            padding: 5px;
-            border-radius: 10px;
-            cursor: pointer;
-            color: black;' 
-            href='/index.html'>Попробовать снова</a>
-      </div>";
-   }
-
-   if($login !== $_POST['login']) {
-      echo 
-      "<div style='width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column;'>
-         <h2>Неверный логин!</h2>
-         <a style='display: inline-block; 
-            text-decoration: none; 
-            background: #cbf95a;
-            padding: 5px;
-            border-radius: 10px;
-            cursor: pointer;
-            color: black;' 
-            href='/index.html'>Попробовать снова</a>
-      </div>";
-   }
-
-   if($pass !== $_POST['pass']) {
-      echo 
-      "<div style='width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column;'>
-         <h2>Неверный пароль!</h2>
-         <a style='display: inline-block; 
-            text-decoration: none; 
-            background: #cbf95a;
-            padding: 5px;
-            border-radius: 10px;
-            cursor: pointer;
-            color: black;' 
-            href='/index.html'>Попробовать снова</a>
+      "<div class='fault'>
+         <h2>Неверный логин или пароль!</h2>
+         <a class='fault-btn' href='/index.html'>Попробовать снова</a>
       </div>";
    }
 
    if($login === $_POST['login'] && $pass === $_POST['pass']) {
       echo 
-      "<div style='width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column;'>
+      "<div class='fault'>
          <h2>Добро пожаловать, ${name} </h2>
-         <a style='display: inline-block; 
-            text-decoration: none; 
-            background: #cbf95a;
-            padding: 5px;
-            border-radius: 10px;
-            cursor: pointer;
-            color: black;' 
-            href='/index.html'>Войти как другой пользователь</a>
+         <a class='fault-btn' href='/index.html'>Войти как другой пользователь</a>
       </div>";
    }
 
