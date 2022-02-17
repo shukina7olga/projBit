893bd7aedb1238ba226bdc833c48af466db1f3c6
shukina7olga <olga.shchukina.99@mail.ru>
2022-02-17 16:53:55 +0300

    task2 version2 fetch

--- ajax.js
+++ ajax.js
@@ -1,40 +1,46 @@
-const errorMessage = 'что-то не так',
-    loadMessage = "загрузка",
-    loginMessage = "вы вошли",
-    loginId = document.getElementById('loginId'),
-    passId = document.getElementById('passId');
+//  errorMessage = 'что-то не так',
+//     loadMessage = "загрузка",
+//     loginMessage = "вы вошли",
 
+console.log('work');
 const form = document.getElementById('formAj');
-
-const message = document.createElement('div');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    form.appendChild(message);
 
+    let divResp = document.createElement('div');
+    divResp.innerHTML = "<b>Hello world!</b>";
+
+   fetch('backend.php', {
+        method: 'POST',
+        body: new FormData(form)
+      })
+      .then((response) => response.text())
+      .then((text) => form.innerHTML = text);
+}) 
+
+//form.append(divResp);
+    //document.innerHTML = divResp;
+   
     // создаем объект экземпляра класса XMLHttpRequest
-    const request = new XMLHttpRequest()
-    request.addEventListener('readystatechange', () => {
-        message.textContent = loadMessage;
-
-        if(request.readyState !== 4) { 
-            return;
-        }
-        if(request.status === 200){ // всё ок
-            message.textContent = loginMessage;
-            loginId.value = '';
-            passId.value ='';
-        }else {
-            message.textContent = errorMessage;
-        }
-    })
-    request.open('POST', 'backend.php');
-    request.setRequestHeader('Content-Type', 'multipart/form-data'); // устанавливает значение полей для отсылаемых данных
+    // const request = new XMLHttpRequest()
+    // request.addEventListener('readystatechange', () => {
+       
+    //     if(request.readyState !== 4) { 
+    //         return;
+    //     }
+    //     if(request.status === 200){ // всё ок
+    //         let resp = request.response;
+    //         divResp.innerHTML = resp;
+    //     }
+    // })
+    // request.open('POST', 'backend.php');
+    // request.setRequestHeader('Content-Type', 'multipart/form-data'); // устанавливает значение полей для отсылаемых данных
     // перед отправкой получаем данные 
-    const formData = new FormData(form);
-    //отправляем при помощи метода send
-    request.send(formData);
-
-
-
-})
+    // const formData = new FormData();
+    // let inputs = document.querySelectorAll('input');
+    // inputs.forEach((input) => {
+    //     formData.append(input.name, input.value);
+    // });
+    // //отправляем при помощи метода send
+    // request.send(formData);
--- backend.php
+++ backend.php
@@ -1,24 +1,22 @@
 <?php 
-   // echo "<link rel='stylesheet' href='style.css'>";
+   $name = 'Bars';
+   $login = 'barsum';
+   $pass = '123';
 
-   // $name = 'Bars';
-   // $login = 'barsum';
-   // $pass = '123';
-
-   // if($pass !== $_POST['pass'] || $login !== $_POST['login']) {
-   //    echo 
-   //    "<div class='fault'>
-   //       <h2>Неверный логин или пароль!</h2>
-   //       <a class='fault-btn' href='/index.html'>Попробовать снова</a>
-   //    </div>";
-   // } else {
-   //    echo 
-   //    "<div class='fault'>
-   //       <h2>Добро пожаловать, ${name} </h2>
-   //       <a class='fault-btn' href='/index.html'>Войти как другой пользователь</a>
-   //    </div>";
-   // }
-   echo $_SERVER['REMOTE_ADDR'];
+   if($pass === $_POST['pass'] || $login === $_POST['login']) {
+      ?>
+      <div class='fault'>
+         <h2>Добро пожаловать, <?=$name?></h2>
+         <a class='fault-btn' href='/index.html'>Попробовать снова</a>
+      </div>
+      <?php 
+      } else { ?>
+      <div class='fault'>
+         <h2>Неверный логин или пароль</h2>
+         <a class='fault-btn' href='/index.html'>Войти как другой пользователь</a>
+      </div>
+   <?php 
+   }
 
 ?> 
 
