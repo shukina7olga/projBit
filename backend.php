af93efc51ba67afdcc5b01bad3880ec0e23284e7
shukina7olga <olga.shchukina.99@mail.ru>
2022-03-03 15:15:02 +0300

    task5 version1

--- backend.php
+++ backend.php
@@ -1,17 +1,23 @@
 <?php 
-   $name = 'Bars';
-   $login = 'barsum';
-   $pass = '123';
-   $fullName = 'Иванов Иван Васильевич';
 
+   $pass = $_POST['pass'];
+   $login = $_POST['login'];
+
+   
+   $mysql = new mysqli('127.0.0.1', 'root', 'password', 'mybit');
+   $result = $mysql->query("SELECT * FROM `users` WHERE `user_login` = '$login' AND `user_pass` = '$pass'"); // записи в  формате объекта
+   $user = $result->fetch_assoc(); // конвертируем данные в массив
+   
+
+   $fullName = $user['user_fullName'];
 
    $arr = array(
       'status' => 'error',
       'data' => [],
       'message' => ''
-   );
+   ); 
    
-   if($pass === $_POST['pass'] && $login === $_POST['login']) {     
+   if(count($user) !== 0) {     
       $arr['status'] = 'success';
       $arr['data'] = [
          'full_name' => $fullName
@@ -23,6 +29,7 @@
    }
 
   echo $json = json_encode($arr, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+
+  $mysql->close(); // закрыть соединение
    
 ?> 
-
--- usersCreate.sql
+++ usersCreate.sql
@@ -4,8 +4,8 @@ create table users
  /*user_id - первичный ключ, целое число с 
 автоинкрементом (+1), который никогда не будет равен нулю*/
  user_id int NOT NULL auto_increment primary key,
- user_name varchar(20) NOT NULL, /*имя */
- user_fullName varchar(50) NOT NULL, /*фио */
- user_login varchar(20) NOT NULL, /*логин*/
- user_pass int(10) NOT NULL /*пароль*/
+ user_name varchar(255) NOT NULL, /*имя */
+ user_fullName varchar(255) NOT NULL, /*фио */
+ user_login varchar(255) NOT NULL, /*логин*/
+ user_pass char(255) NOT NULL /*пароль*/
 );
